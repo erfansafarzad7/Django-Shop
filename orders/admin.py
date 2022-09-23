f931a508b8e37b30cf01c9733474352cc2997885
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-09-23 12:12:08 +0330

    coupon

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Coupon
 
 
 class OrderItemInline(admin.TabularInline):
@@ -12,4 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'updated', 'paid')
     list_filter = ('paid', )
     inlines = (OrderItemInline, )
+
+
+admin.site.register(Coupon)
 
